2f73545aa714806383662f6908956257abc419f6
Giovistica <giovi.rhcp@gmail.com>
2023-11-06 14:40:11 -0300

    cambio en enum status

--- src/enums/status.enum.ts
+++ src/enums/status.enum.ts
@@ -3,5 +3,6 @@ export enum Status {
   send = 'SEND', //gris claro el cliente envio el pedido
   cancelled = 'CANCELLED', //rojo el local lo rechazo
   accepted = 'ACCEPTED', //amarillo el local lo acepto
+  received = 'RECEIVED', //el clente lo recibio
   delivered = 'DELIVERED', //verde oscuro el cliente lo recibio
 }
